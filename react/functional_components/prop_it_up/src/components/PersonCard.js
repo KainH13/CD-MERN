b757a9b60a8320aa5af88319737ccb2f08e75ae5
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2021-12-07 15:52:05 -0800

    react - completed Putting it Together assignment

--- react/functional_components/prop_it_up/src/components/PersonCard.js
+++ react/functional_components/prop_it_up/src/components/PersonCard.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // bootstrap
 import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 const PersonCard = (props) => {
     const { firstName, lastName, age, hairColor } = props;
+    const [currentAge, setAge] = useState(age)
+    const addAge = () => {
+      setAge(currentAge + 1)
+    }
     return (
       <Card className="col-6 offset-3 p-2 mt-1">
         <h1>{ lastName }, { firstName }</h1>
-        <p>Age: { age }</p>
+        <p>Age: { currentAge }</p>
         <p>Hair Color: { hairColor }</p>
+        <Button variant="outline-primary" onClick={ addAge }>Birthday!</Button>
       </Card>
     );
 }
