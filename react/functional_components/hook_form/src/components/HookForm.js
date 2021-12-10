89efc96db1fea4c10150702bef82e1389a908a84
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2021-12-09 16:49:44 -0800

    react - More Forms assignment + sensei bonus

--- react/functional_components/hook_form/src/components/HookForm.js
+++ react/functional_components/hook_form/src/components/HookForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 // bootstrap
 import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
 
 const HookForm = (props) => {
   const [firstName, setFirstName] = useState("");
@@ -18,17 +19,21 @@ const HookForm = (props) => {
 
   const [confirmPassword, setConfirmPassword] = useState("");
   const [confirmPasswordError, setConfirmPasswordError] = useState("");
-
+  
+  // important to remember -- setting state variables is an async process, so logging them isn't too helpful as they could lag behind the value of e.target.value
   const handleFirstName = (e) => {
-    if (e.target.value.length < 2) {
+    setFirstName(e.target.value);
+    console.log("firstName = ", e.target.value)
+    if (e.target.value.length > 0 && e.target.value.length < 2) {
       setFirstNameError("First name must be at least 2 characters.");
     } else {
       setFirstNameError("");
     }
   };
-
+  
   const handleLastName = (e) => {
-    if (e.target.value.length < 2) {
+    setLastName(e.target.value);
+    if (e.target.value.length > 0 && e.target.value.length < 2) {
       setLastNameError("Last name must be at least 2 characters.");
     } else {
       setLastNameError("");
@@ -36,7 +41,8 @@ const HookForm = (props) => {
   };
 
   const handleEmail = (e) => {
-    if (e.target.value.length < 2) {
+    setEmail(e.target.value);
+    if (e.target.value.length > 0 && e.target.value.length < 2) {
       setEmailError("Email must be at least 2 characters.");
     } else {
       setEmailError("");
@@ -44,88 +50,120 @@ const HookForm = (props) => {
   };
 
   const handlePassword = (e) => {
-    if (e.target.value.length < 8) {
-      setPasswordError("Password must be at least 2 characters.");
+    setPassword(e.target.value);
+    if (e.target.value.length > 0 && e.target.value.length < 8) {
+      setPasswordError("Password must be at least 8 characters.");
     } else {
       setPasswordError("");
     }
   };
 
   const handleConfirmPassword = (e) => {
-    if (e.target.value.length !== password) {
+    if (e.target.value !== password) {
       setConfirmPasswordError("Passwords do not match.");
     } else {
       setConfirmPasswordError("");
     }
   };
-
+  
   return (
     <div>
       <Form className="col-sm-6 offset-sm-3 mt-2">
+
+        {
+          firstNameError ?
+          <Alert variant="danger">{firstNameError}</Alert> :
+          ''          
+        }
         <Form.Group
           className="mb-3 row align-items-center"
           controlId="firstName"
-        >
+          >
           <Form.Label className="col-sm-3">First Name:</Form.Label>
           <div className="col-sm-9">
             <Form.Control
               type="text"
-              onChange={(e) => setFirstName(e.target.value)}
-            />
+              onChange={ handleFirstName }
+              />
           </div>
         </Form.Group>
+
+        {
+          lastNameError ?
+          <Alert variant="danger">{lastNameError}</Alert> :
+          ''          
+        }
         <Form.Group
           className="mb-3 row align-items-center"
-          controlId="firstName"
+          controlId="lastName"
         >
           <Form.Label className="col-sm-3">Last Name:</Form.Label>
           <div className="col-sm-9">
             <Form.Control
               type="text"
-              onChange={(e) => setLastName(e.target.value)}
+              onChange={handleLastName}
             />
           </div>
         </Form.Group>
+
+        {
+          emailError ?
+          <Alert variant="danger">{emailError}</Alert> :
+          ''          
+        }
         <Form.Group
           className="mb-3 row align-items-center"
-          controlId="firstName"
+          controlId="email"
         >
           <Form.Label className="col-sm-3">Email:</Form.Label>
           <div className="col-sm-9">
             <Form.Control
               type="email"
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={handleEmail}
             />
           </div>
         </Form.Group>
-        <Form.Group className="mb-3 row align-items-center" controlId="email">
+
+        {
+          passwordError ?
+          <Alert variant="danger">{passwordError}</Alert> :
+          ''          
+        }
+        <Form.Group className="mb-3 row align-items-center" controlId="password">
           <Form.Label className="col-sm-3">Password:</Form.Label>
           <div className="col-sm-9">
             <Form.Control
               type="password"
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handlePassword}
             />
           </div>
         </Form.Group>
-        <Form.Group className="mb-3 row align-items-center" controlId="email">
+
+        {
+          confirmPasswordError ?
+          <Alert variant="danger">{confirmPasswordError}</Alert> :
+          ''          
+        }
+        <Form.Group className="mb-3 row align-items-center" controlId="confirmPassword">
           <Form.Label className="col-sm-3">Confirm Password:</Form.Label>
           <div className="col-sm-9">
             <Form.Control
               type="password"
-              onChange={(e) => setConfirmPassword(e.target.value)}
+              onChange={handleConfirmPassword}
             />
           </div>
         </Form.Group>
+
       </Form>
 
-      <div className="col-sm-3 offset-sm-5 mt-2">
+      {/* <div className="col-sm-3 offset-sm-5 mt-2">
         <h1>Your form data</h1>
         <p>First Name: {firstName}</p>
         <p>Last Name: {lastName}</p>
         <p>Email: {email}</p>
         <p>Password: {password}</p>
         <p>Confirm Password: {confirmPassword}</p>
-      </div>
+      </div> */}
     </div>
   );
 };
