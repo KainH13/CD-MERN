005e2d0cf2026153c7917eb206359495b99049df
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2021-12-14 10:12:24 -0800

    react - box generator - layout complete

--- react/functional_components/box_generator/src/components/Boxes.js
+++ react/functional_components/box_generator/src/components/Boxes.js
@@ -3,11 +3,25 @@ import React from "react";
 // bootstrap
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 const Boxes = (props) => {
+  const colors = ["red", "blue", "green", "yellow", "orange", "purple"]
+
   return (
-    <div>boxes</div>
+    <Container>
+      <Row className="offset-2">
+        {
+          colors.map( (color, index) =>
+            <div 
+            style={{backgroundColor: color, height: "150px"}}
+            className="col-3 m-2" 
+            key={ index }>
+              { color }
+            </div>
+          )
+        }
+      </Row>
+    </Container>
   );
 };
 
--- react/functional_components/box_generator/src/components/ColorForm.js
+++ react/functional_components/box_generator/src/components/ColorForm.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 // bootstrap
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 const ColorForm = (props) => {
+  const [color, setColor] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.newBlock( color )
+  };
+
   return (
-    <Form className="col-sm-6 offset-sm-3 mt-2">
+    <Form onSubmit={ handleSubmit } className="col-sm-6 offset-sm-3 mt-2">
       <Form.Group className="mb-3 row align-items-center" controlId="color">
         <Form.Label className="col-sm-3">Color:</Form.Label>
         <div className="col-sm-9">
-          <Form.Control type="text"/>
+          <Form.Control type="text" onChange={ (e) => setColor(e.target.value) }/>
         </div>
       </Form.Group>
       <Button className="col-sm-11 offset-sm-1" variant="primary" type="submit">
-        Add
+        Add Block
       </Button>
     </Form>
   );
