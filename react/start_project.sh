fd94f6db0319a07eb9ba703a9cb9e9898cfc6576
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2021-12-21 17:29:29 -0800

    react - routing - practice setup

--- react/start_project.sh
+++ react/start_project.sh
@@ -1,6 +1,13 @@
 # in directory where you wish the project folder to live
 npx create-react-app [your-project-name-here]
 
+# all else must happen in the directory that was created by CRA
+cd [your-project-name-here]
+
+# add packages needed for full stack
+npm install axios
+npm install @reach/router
+
 # add react-bootstrap
 npm install react-bootstrap bootstrap@5.1.3
 
