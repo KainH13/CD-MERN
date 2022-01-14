4a768e3d02e8a71c36a0af9015c5e65d49e99bcb
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2022-01-14 12:19:06 -0800

    added front end to full stack example

--- Full_Stack_MERN/full_stack/newProject/client/src/components/PersonForm.js
+++ Full_Stack_MERN/full_stack/newProject/client/src/components/PersonForm.js
@@ -2,18 +2,48 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const PersonForm = () => {
-    const [message, setMessage] = useState("Loading...");
-    useEffect(() => {
+    // use state to track form input values
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
+    // handle form submission
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        // post to create new person
         axios
-            .get("http://localhost:8000/api")
-            .then((res) => setMessage(res.data.message))
+            .post("http://localhost:8000/api/people", {
+                firstName,
+                lastName,
+            })
+            .then((res) => {
+                console.log(res);
+
+                console.log(res.data);
+            })
             .catch((err) => console.log(err));
-    }, []);
+    };
 
     return (
-        <div>
-            <h2>Message from the backend: {message}</h2>
-        </div>
+        <form onSubmit={onSubmitHandler}>
+            <p>
+                <label>First Name</label>
+                <br />
+                <input
+                    type="text"
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+            </p>
+            <p>
+                <label>Last Name</label>
+                <br />
+                <input
+                    type="text"
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+            </p>
+            <input type="submit" />
+        </form>
     );
 };
 
