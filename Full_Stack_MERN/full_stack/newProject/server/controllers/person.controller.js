2320ddeb81dde1143669d4dcbb79d19521342817
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2022-01-14 12:05:46 -0800

    newProject - updated template MongoDB connection via Mongoose

--- Full_Stack_MERN/full_stack/newProject/server/controllers/person.controller.js
+++ Full_Stack_MERN/full_stack/newProject/server/controllers/person.controller.js
@@ -1,5 +1,15 @@
-module.exports.index = (request, response) => {
-    response.json({
-        message: "Hello World",
-    });
+const Person = require("../models/person.model");
+
+module.exports = {
+    index: (request, response) => {
+        response.json({
+            message: "Hello World",
+        });
+    },
+
+    createPerson: (request, response) => {
+        Person.create(request.body)
+            .then((person) => response.json(person))
+            .catch((err) => response.json(err));
+    }
 };
--- Full_Stack_MERN/full_stack/newProject/server/server.js
+++ Full_Stack_MERN/full_stack/newProject/server/server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
 const port = 8000;
 
+// enabling cors and json
+app.use(cors());
+app.use(express.json(), express.urlencoded({ extended: true }));
+
+// project specific requirements
+require("./config/mongoose.config");
 require("./routes/person.routes")(app);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
