f9f06ed2e881d3dfe3ee477a03c5ab8db096f5c6
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2022-01-15 14:48:21 -0800

    added getAllPeople API functionality

--- Full_Stack_MERN/full_stack/newProject/server/controllers/person.controller.js
+++ Full_Stack_MERN/full_stack/newProject/server/controllers/person.controller.js
@@ -1,15 +1,21 @@
 const Person = require("../models/person.model");
 
 module.exports = {
-    index: (request, response) => {
-        response.json({
-            message: "Hello World",
-        });
-    },
-
     createPerson: (request, response) => {
         Person.create(request.body)
             .then((person) => response.json(person))
             .catch((err) => response.json(err));
+    },
+
+    getAllPeople: (request, response) => {
+        Person.find({})
+            .then(persons => {
+                console.log(persons);
+                response.json(persons);
+            })
+            .catch(err => {
+                console.log(err);
+                response.json(err);
+            })
     }
 };
