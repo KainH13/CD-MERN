073caa0e12a428b03cb56536bc232d5b14b56fdf
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2022-01-17 16:31:45 -0800

    syntax fix

--- Full_Stack_MERN/full_stack/newProject/client/src/components/Update.js
+++ Full_Stack_MERN/full_stack/newProject/client/src/components/Update.js
@@ -29,8 +29,8 @@ const Update = (props) => {
             })
             .then((res) => console.log(res))
             .catch((err) => console.log(err));
-        
-        navigate("/people")
+
+        navigate("/people");
     };
 
     return (
